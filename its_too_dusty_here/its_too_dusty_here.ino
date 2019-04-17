254dc3fde9ec3dc20fae4f2f131af6ca3f3415ba
PaulNicolasHunter <choudhary.vivek98@gmail.com>
2019-04-16 23:08:20 -0500

    added creds

--- its_too_dusty_here/its_too_dusty_here.ino
+++ its_too_dusty_here/its_too_dusty_here.ino
@@ -19,8 +19,16 @@ int gas_sens = A2;
 float r0, ratio;
 unsigned int sampling_time = 280;
 unsigned int sleep_time = 9680;
-char *gases[TOTAL_GASES_IDENTIFY] = {"LPG", "CO", "Smoke"};
-float gas_curve_data[TOTAL_GASES_IDENTIFY][3] = {{2.3, 0.21, -0.47}, {2.3, 0.72, -0.34}, {2.3, 0.53, -0.44}}; // points are taken from the curve.
+char *gases[TOTAL_GASES_IDENTIFY] = {
+  "LPG", "CO", "Smoke"};
+float gas_curve_data[TOTAL_GASES_IDENTIFY][3] = {
+  {
+    2.3, 0.21, -0.47    }
+  , {
+    2.3, 0.72, -0.34    }
+  , {
+    2.3, 0.53, -0.44    }
+}; // points are taken from the curve.
 
 // code serving constituents
 int i;
@@ -31,7 +39,8 @@ struct gas_reads {
   char *gas[TOTAL_GASES_IDENTIFY];
   float reading[TOTAL_GASES_IDENTIFY];
 
-} air_;
+} 
+air_;
 
 void setup() {
 
@@ -46,28 +55,32 @@ void loop() {
   dust_samp = read_dust();
   ratio = gaseous_air_res_samples();
   gas_digest(ratio);
-  
+
   Serial.println(dust_samp);
-  
+
   if (dust_samp >= 0.10) {
+
     analogWrite(fan_out, 255);
-  } else {
+
+  } 
+  else {
     analogWrite(fan_out, 0);
+
   }
 
   /*
     gpio logic goes here
-  */
+   */
 
-    Serial.println("{\"dust\":" + String(dust_samp) + ",\"0\":{\"gas\":" + String(air_.gas[0]) + ",\"reading\":" + String(air_.reading[0]) + "},\"1\":{\"gas\":" + String(air_.gas[1]) + ",\"reading\":" + String(air_.reading[1]) + "},\"2\":{\"gas\":" + String(air_.gas[2]) + ",\"reading\":" + String(air_.reading[2]) + "}}");
+  Serial.println("{\"dust\":" + String(dust_samp) + ",\"0\":{\"gas\":" + String(air_.gas[0]) + ",\"reading\":" + String(air_.reading[0]) + "},\"1\":{\"gas\":" + String(air_.gas[1]) + ",\"reading\":" + String(air_.reading[1]) + "},\"2\":{\"gas\":" + String(air_.gas[2]) + ",\"reading\":" + String(air_.reading[2]) + "}}");
 }
 
 /* Dust sensor reading */
 float read_dust() {
   /* *-*
-    The LED control pin (pin 3 on the SHARP dust sensor module) is active low.
-    This means that a LOW ( 0V ) value will turn the LED on and a HIGH ( VCC ) value will turn the LED off.
-    -** */
+   The LED control pin (pin 3 on the SHARP dust sensor module) is active low.
+   This means that a LOW ( 0V ) value will turn the LED on and a HIGH ( VCC ) value will turn the LED off.
+   -** */
 
   digitalWrite(dust_led, LOW); // turning the led to ACTIVE HIGH
   delayMicroseconds(sampling_time); // delay for pulse mapping [1]
@@ -122,3 +135,5 @@ float gas_digest(float ratios) {
 float gas_percent(float ratios, float *line_curve) {
   return pow(10, ( ((log(ratios) - line_curve[1]) / line_curve[2]) + line_curve[0]));
 }
+
+
